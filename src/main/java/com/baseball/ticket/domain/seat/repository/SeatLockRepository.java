9fbd0138290fe03d0355058ffbde0b63702eb739
rkskekfk0714 <rkskekfk0714@gmail.com>
2026-04-30 14:02:09 +0900

    feat: 경기/좌석/대기열 API 구현 (Redis 분산락, WebSocket)

--- src/main/java/com/baseball/ticket/domain/seat/repository/SeatLockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baseball.ticket.domain.seat.repository;
-
-import com.baseball.ticket.domain.seat.entity.SeatLock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatLockRepository
-        extends JpaRepository<SeatLock, String> {
-}
