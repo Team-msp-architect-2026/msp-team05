69d4d0550aff5b21b9b09f50cd763b847ac5cbfa
김수경 <rkskekfk0714@gmail.com>
2026-06-06 12:50:01 +0900

    feat(infra): ECR 모듈 추가
    - kky-prod-backend ECR 리포지토리 생성
    - scan_on_push 활성화
    - ECR ReadOnly IAM 권한 EC2에 추가

---
+++ modules/ecr/output.tf
@@ -0,0 +1,9 @@
+output "repository_url" {
+  description = "ECR Repository URL"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+output "repository_name" {
+  description = "ECR Repository Name"
+  value       = aws_ecr_repository.backend.name
+}
